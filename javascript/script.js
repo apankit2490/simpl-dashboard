5fb34d66f72c67f8af835144334b8f329596d2f8
Ankit Kumar patnaik <ankit.patnaik@hashedin.com>
2019-04-30 00:02:10 +0530

    Completed profile box and added flex components

--- javascript/script.js
+++ javascript/script.js
@@ -2,16 +2,24 @@
       var myDoughnutChart = new Chart(mychart, {
       type: 'doughnut',
       data: {
-        // labels:['food','personal care','cloths','travel'],
+        labels:['food','personal care','cloths','travel'],
         datasets:[{
           label:'statistics',
           data:[
             111,222,333,444
           ],
-          backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"]
+          backgroundColor: ["#ff5950", "#ff9600", "#77898a", "#00d1c1"]
         }]
       },
       options: {
-        responsive: false,
-        }
+        responsive: true,
+        legend: {
+            display: false
+        },
+          maintainAspectRatio: false,
+      }
   });
+  
+
+  
+  
